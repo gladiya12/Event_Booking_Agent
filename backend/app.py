1d953b4ca28f6c73af44c345147342c434e1c2ce
gladiya12 <gladiyathiyagu128@gmail.com>
2026-06-11 09:00:26 +0530

    Implemented event list

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
+import json
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/")
 def home():
-    return {
+    return jsonify({
         "message": "Backend Running Successfully"
-    }
+    })
+
+@app.route("/events")
+def get_events():
+
+    with open("data/events.json", "r") as file:
+        events = json.load(file)
+
+    return jsonify(events)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,48 @@
 import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-
-  const [message, setMessage] = useState("");
+  const [events, setEvents] = useState([]);
 
   useEffect(() => {
-    fetch("http://127.0.0.1:5000/")
+    fetch("http://127.0.0.1:5000/events")
       .then((response) => response.json())
       .then((data) => {
-        setMessage(data.message);
+        setEvents(data);
       });
   }, []);
 
   return (
-    <div style={{ textAlign: "center", marginTop: "100px" }}>
+    <div className="container">
       <h1>Event Booking Agent</h1>
 
-      <h2>Backend Status:</h2>
+      <h2>Top Events</h2>
 
-      <p>{message}</p>
+      <div className="event-grid">
+        {events.map((event) => (
+          <div className="event-card" key={event.id}>
+            <h3>{event.name}</h3>
+
+            <p>
+              <strong>Category:</strong> {event.category}
+            </p>
+
+            <p>
+              <strong>Date:</strong> {event.date}
+            </p>
+
+            <p>
+              <strong>Venue:</strong> {event.venue}
+            </p>
+
+            <p>
+              <strong>Price:</strong> ₹{event.price}
+            </p>
+
+            <button>View Details</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
